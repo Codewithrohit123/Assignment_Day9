d597b6eb1bc1cc6b78daf37895ee9ff6c36d43bc
Rohit Jaiswal <rohitjaiswal17398@gmail.com>
2024-12-22 12:55:58 +0530

    Add files via upload

---
+++ Alphabet.java
@@ -0,0 +1,8 @@
+package Assignment_Day10;
+public class Alphabet {
+    public static void main(String[] args) {
+        for (char c = 'A'; c <= 'Z'; c++) {
+            System.out.print(c + " "); 
+        }
+    }
+}
